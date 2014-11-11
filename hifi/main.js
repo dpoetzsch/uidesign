c434b31d4e4cb513e608e38dd90ca3512ea715ac
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-11-11 22:46:27 +0100

    more work on basic template

---
+++ hifi/main.js
@@ -0,0 +1,8 @@
+function loginClicked() {
+  var elem = document.getElementById('login-fields');
+  if (elem.style.visibility == "visible") {
+    elem.style.visibility = "hidden";
+  } else {
+    elem.style.visibility = "visible";
+  }
+}

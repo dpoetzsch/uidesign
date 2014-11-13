45fd8d77ceea28f9e4d40be32fcf088a39912f16
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-11-13 19:40:15 +0100

    automated item lists

--- hifi/assemble.rb
+++ hifi/assemble.rb
@@ -3,6 +3,58 @@
 require "fileutils"
 
 template = File.read("template.html")
+ITEMLIST = File.read("itemlist.html")
+COMMENT_DIV = File.read("comment-div.html")
+
+def convert_itemlist(il)
+  # convert to list
+  list = []
+  cur = nil
+  il.each_line { |l|
+    if l[0] == "*"
+      # type, name, comments list
+      cur = [l.split[1], l.split[2..-1].join(" "), []]
+      list.push cur
+    elsif not cur.nil?
+      cur[-1].push(l.strip.split(" -- "))
+    end
+  }
+  
+  html = ""
+  list.each_with_index do |item, idx|
+    t = ITEMLIST.gsub("$ITEMTYPE$", item[0])
+    t.gsub!("$TITLE$", item[1])
+    t.gsub!("$COMM-NUM$", item[2].length.to_s)
+    t.gsub!("$ITEM-IDX$", idx.to_s)
+    
+    comments = ""
+    item[2].each_with_index do |comment, cidx|
+      comments += COMMENT_DIV.gsub("$TEXT$", comment[0]).gsub("$DATE$", comment[1])
+    end
+    t.gsub!("$COMMENTS-AREA$", comments)
+    
+    html += t
+  end
+  
+  return html
+end
+
+def preproc_itemlist(str)
+  l = str.split(/^=BEGIN ITEMLIST$/)
+  if l.length > 1
+    nstr = l[0]
+    l[1..-1].each { |e|
+      l2 = e.split(/^=END ITEMLIST$/)
+      raise unless l2.length == 2
+      
+      nstr += convert_itemlist(l2[0])
+      nstr += l2[1]
+    }
+    return nstr
+  else
+    return str
+  end
+end
 
 begin
   FileUtils.mkdir("output")
@@ -26,6 +78,9 @@ Dir["content/*.html"].each { |f|
   
   html = template.clone
   repl_map.each { |k, v|
+    # preprocess value
+    v = preproc_itemlist(v)
+  
     html.gsub!(k, v)
   }
   
--- hifi/output/main.js
+++ hifi/output/main.js
@@ -13,11 +13,11 @@ function commentClicked(id) {
   
   if (link.text.substring(0,1) == String.fromCharCode('09654')) { // closed
     // open it
-    comments.style.visibility = "visible";
+    comments.style.display = "inline";
     link.text = String.fromCharCode('0x25bc') + link.text.substring(1);
   } else {
     // close it
-    comments.style.visibility = "hidden";
+    comments.style.display = "none";
     link.text = String.fromCharCode('09654') + link.text.substring(1);
   }
 }

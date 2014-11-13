568fe868dec1a1f5186090f3809c2dedfc37a418
David Poetzsch-Heffter <davidpoetzsch@web.de>
2014-11-13 18:53:50 +0100

    added search result

--- hifi/output/main.js
+++ hifi/output/main.js
@@ -6,3 +6,18 @@ function loginClicked() {
     elem.style.visibility = "visible";
   }
 }
+
+function commentClicked(id) {
+  var link = document.getElementById("comment-link-" + id);
+  var comments = document.getElementById("comment-area-" + id);
+  
+  if (link.text.substring(0,1) == String.fromCharCode('09654')) { // closed
+    // open it
+    comments.style.visibility = "visible";
+    link.text = String.fromCharCode('0x25bc') + link.text.substring(1);
+  } else {
+    // close it
+    comments.style.visibility = "hidden";
+    link.text = String.fromCharCode('09654') + link.text.substring(1);
+  }
+}
